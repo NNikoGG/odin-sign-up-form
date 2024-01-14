a5bfa853fb995822833aa8eeeea0c0427147e979
NNikoGG <geekgaming222@gmail.com>
2024-01-14 16:07:06 +0530

    Fixed a bug with the length error display message

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add input event listener to confirmpassword field
     confirmPasswordInput.addEventListener('input', function() {
         const errorTextContainer = document.querySelector('.confirm-password-error-text');
+        const lengthErrorContainer = document.querySelector('.password-error-text');
         while (errorTextContainer.firstChild) {
             errorTextContainer.removeChild(errorTextContainer.firstChild);
+        }
+        while (lengthErrorContainer.firstChild) {
+            lengthErrorContainer.removeChild(lengthErrorContainer.firstChild);
+        }
+
+        if (passwordInput.value.length < 8){
+            const lengthError = document.createElement('span');
+            lengthError.style.color = 'red';
+            lengthError.textContent = '*Password must be 8 or more characters';
+            lengthErrorContainer.appendChild(lengthError);
         }
         
         if (confirmPasswordInput.value !== passwordInput.value) {
@@ -16,12 +27,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const errorText = document.createElement('span');
             errorText.style.color = 'red';
             errorText.textContent = '*Password did not match';
-            
             errorTextContainer.appendChild(errorText);
-        } else {
+        } 
+
+        else {
             passwordInput.style.outlineColor = '';
             confirmPasswordInput.style.outlineColor = '';
         }
-        
     });
 });
